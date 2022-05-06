0407b57f1b70eb14cc15494254dba6cd7da7eb6c
Edoardo Barbieri <edoardo@twinlogix.com>
2022-05-06 14:16:22 +0200

    fix: codegen

--- autogenerated-resolvers/src/utils.ts
+++ autogenerated-resolvers/src/utils.ts
@@ -1,14 +1,15 @@
 import { DAOContext } from './dao.generated'
 import { v4 as uuid } from 'uuid'
 
-export async function getDao(): Promise<DAOContext> {
+export type ExampleDAOContext = DAOContext
+export async function getDao(): Promise<ExampleDAOContext> {
   const dao = new DAOContext({
     scalars: {
       ID: { generate: () => uuid() },
-      Date: { generate: () => new Date() }
+      Date: { generate: () => new Date() },
     },
     log: async (args) => {
-      console.log(args.operation, args.dao, JSON.parse(args.query ?? '{}')?.filter)
+      console.log(args.operation, args.dao, JSON.parse(args.query ?? '{}')?.filter, JSON.parse(args.query ?? '{}')?.sorts)
     },
   })
   return dao
