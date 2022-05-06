b477b9e7e7867172e1c2e21b242e8fff31308383
Edoardo Barbieri <edoardo@twinlogix.com>
2022-05-06 15:34:50 +0200

    chore: improve examples

--- autogenerated-resolvers/src/index.ts
+++ autogenerated-resolvers/src/index.ts
@@ -1,16 +1,33 @@
-import 'graphql-import-node';
+import 'graphql-import-node'
 import { ApolloServer } from 'apollo-server'
-import { resolvers } from './resolvers.generated'
-import { getDao } from './utils'
+import { resolvers } from './operations.generated'
 import { mergeTypeDefs } from '@graphql-tools/merge'
-import inputTypeDefs from './inputs.generated.graphql'
+import inputTypeDefs from './operations.types.generated.graphql'
 import schemaTypeDefs from './schema.graphql'
 import { typeDefs as typettaDirectivesTypeDefs } from '@twinlogix/typetta'
+import { DAOContext } from './orm.generated'
+import { v4 as uuid } from 'uuid'
+
+export type Context = {
+  entityManager: DAOContext
+}
+
+async function createContext(): Promise<Context> {
+  return {
+    entityManager: new DAOContext({
+      scalars: {
+        ID: { generate: () => uuid() },
+        Date: { generate: () => new Date() },
+      },
+      log: true,
+    }),
+  }
+}
 
 const server = new ApolloServer({
   typeDefs: mergeTypeDefs([inputTypeDefs, schemaTypeDefs, typettaDirectivesTypeDefs]),
   resolvers,
-  context: getDao,
+  context: createContext,
 })
 
 server.listen().then(({ url }) => {
